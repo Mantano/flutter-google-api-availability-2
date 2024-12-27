f1064c00c85927632334072a09f1d7b036cb8271
Jean-Marie Geffroy <jmg@mantano.com>
2024-12-27 18:17:08 +0000

    Update GoogleApiAvailabilityPlugin.java

--- google_api_availability_android/android/src/main/java/com/baseflow/googleapiavailability/GoogleApiAvailabilityPlugin.java
+++ google_api_availability_android/android/src/main/java/com/baseflow/googleapiavailability/GoogleApiAvailabilityPlugin.java
@@ -12,71 +12,66 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.PluginRegistry.ViewDestroyListener;
 import io.flutter.view.FlutterNativeView;
 
-/**
- * GoogleApiAvailabilityPlugin
- */
 public class GoogleApiAvailabilityPlugin implements FlutterPlugin, ActivityAware {
 
-  private final GoogleApiAvailabilityManager googleApiAvailabilityManager;
-  private MethodChannel channel;
-  private MethodCallHandlerImpl methodCallHandler;
+    private final GoogleApiAvailabilityManager googleApiAvailabilityManager;
+    private MethodChannel channel;
+    private MethodCallHandlerImpl methodCallHandler;
 
-  public GoogleApiAvailabilityPlugin() {
-      this.googleApiAvailabilityManager = new GoogleApiAvailabilityManager();
-  }
+    public GoogleApiAvailabilityPlugin() {
+        this.googleApiAvailabilityManager = new GoogleApiAvailabilityManager();
+    }
 
-  @Override
-  public void onAttachedToActivity(ActivityPluginBinding binding) {
-    methodCallHandler.setActivity(binding.getActivity());
-  }
+    // Activity lifecycle methods
+    @Override
+    public void onAttachedToActivity(ActivityPluginBinding binding) {
+        if (methodCallHandler != null) {
+            methodCallHandler.setActivity(binding.getActivity());
+        }
+    }
 
-  @Override
-  public void onDetachedFromActivity() {
-    methodCallHandler.setActivity(null);
-  }
+    @Override
+    public void onDetachedFromActivity() {
+        if (methodCallHandler != null) {
+            methodCallHandler.setActivity(null);
+        }
+    }
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    methodCallHandler.setActivity(binding.getActivity());
-  }
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        if (methodCallHandler != null) {
+            methodCallHandler.setActivity(binding.getActivity());
+        }
+    }
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-    methodCallHandler.setActivity(null);
-  }
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        if (methodCallHandler != null) {
+            methodCallHandler.setActivity(null);
+        }
+    }
 
-  @Override
-  public void onAttachedToEngine(FlutterPluginBinding binding) {
-    registerPlugin(binding.getApplicationContext(), binding.getBinaryMessenger());
-  }
+    // Flutter plugin lifecycle methods
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        registerPlugin(binding.getApplicationContext(), binding.getBinaryMessenger());
+    }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    unregisterPlugin();
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        unregisterPlugin();
+    }
 
-/*  public static void registerWith(Registrar registrar) {
-    final GoogleApiAvailabilityPlugin plugin = new GoogleApiAvailabilityPlugin();
-    plugin.registerPlugin(registrar.context(), registrar.messenger());
-    plugin.methodCallHandler.setActivity(registrar.activity());
+    private void registerPlugin(Context context, BinaryMessenger messenger) {
+        methodCallHandler = new MethodCallHandlerImpl(context, googleApiAvailabilityManager);
+        channel = new MethodChannel(messenger, "flutter.baseflow.com/google_api_availability_android/methods");
+        channel.setMethodCallHandler(methodCallHandler);
+    }
 
-    registrar.addViewDestroyListener(new ViewDestroyListener() {
-      @Override
-      public boolean onViewDestroy(FlutterNativeView view) {
-        plugin.unregisterPlugin();
-        return false;
-      }
-    });
-  }
-*/
-  private void registerPlugin(Context context, BinaryMessenger messenger) {
-    methodCallHandler = new MethodCallHandlerImpl(context, googleApiAvailabilityManager);
-    channel = new MethodChannel(messenger, "flutter.baseflow.com/google_api_availability_android/methods");
-    channel.setMethodCallHandler(methodCallHandler);
-  }
-
-  private void unregisterPlugin() {
-    channel.setMethodCallHandler(null);
-    channel = null;
-  }
+    private void unregisterPlugin() {
+        if (channel != null) {
+            channel.setMethodCallHandler(null);
+        }
+        channel = null;
+    }
 }
